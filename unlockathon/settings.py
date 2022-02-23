da693e0e00061b2290056e5f5eb85fb70522c6b4
Ronak-Pradhan <ronakprad@gmail.com>
2022-02-23 23:51:18 +0530

    first commit

--- unlockathon/settings.py
+++ unlockathon/settings.py
@@ -85,12 +85,8 @@ WSGI_APPLICATION = 'unlockathon.wsgi.application'
 
 DATABASES = {
      'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'unlockathon',
-        'USER':'root',
-        'PASSWORD':'',
-        'HOST':'127.0.0.1',
-         'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'unlockathon.db',
     }
 }
 
